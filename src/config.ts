57d3415dac0d077555c6eb358039567a9b74aab5
Vicscript <Victorchinomso80@gmail.com>
2023-01-21 00:49:51 +0100

    SaveNew

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8085;
+
 export const MONGODB_URL = process.env.MONGODB_URL;
+
+export const config = {
+  server: {
+    port: PORT,
+  },
+};
